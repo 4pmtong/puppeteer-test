538a60df8a7966ba85dc56cdcefe5eb387cd7a08
ct <tong_ct_chen@kingdee.com>
2017-11-13 17:07:00 +0800

    ...

--- src/crawler.js
+++ src/crawler.js
@@ -10,9 +10,11 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   let page = await browser.newPage();
   
+  // 跳转到目标网页
   await page.goto('http://brp.kingdee.com/doc/guide');
   await page.waitFor(2 * 1000);
   
+  // 拿到所有技术文档的链接
   let aTags =  await page.evaluate(() => {
     const as = [...document.querySelectorAll('.doc-nav > ul > li > ul > li > a')];
     
@@ -26,10 +28,12 @@ const puppeteer = require('puppeteer');
   
   console.log(aTags);
   
+  // 将当前页面生成pdf
   const guideName = await page.$eval('.doc-nav > ul > li.no-top > a', el => el.text);
   await page.pdf({ path: `data/crawler/${guideName}.pdf` });
   page.close();
   
+  // 跳转到技术文档页面并一一生成pdf
   for (let i = 0; i < aTags.length; i++) {
     page = await browser.newPage();
   
